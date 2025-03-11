14617025d5100449720a8d8a8af8fa622afa197a
yiqian <110219761+yiqianee@users.noreply.github.com>
2025-03-11 17:34:56 +1100

    Added simple navigations to home page

--- server.js
+++ server.js
@@ -1,13 +1,18 @@
 // Import the express module
 const express = require('express');
+const path = require('path'); // Required for file paths
+
 const app = express();
 
 // Define the port number
 const PORT = 3000;
 
-// Define a basic route
+// Serve static files from the "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Define a route for the home page
 app.get('/', (req, res) => {
-  res.send('Weather Visualisation App!');
+  res.sendFile(path.join(__dirname, 'public', 'server.html'));
 });
 
 // Start the server
